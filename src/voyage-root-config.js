774457f153ef26366f50d9b7f6076b1acb28f69b
ubirajarapelli <ubirajaracpelli@gmail.com>
2021-03-15 22:09:50 -0300

    Initial commit and project bootstrap

---
+++ src/voyage-root-config.js
@@ -0,0 +1,57 @@
+import { registerApplication, start } from "single-spa";
+import {
+  constructApplications,
+  constructRoutes,
+  constructLayoutEngine,
+} from 'single-spa-layout';
+
+// registerApplication({
+//   name: "@voyage/main-app",
+//   app: () =>
+//     System.import(
+//       "@voyage/main-app"
+//     ),
+//   activeWhen: ["/"],
+// });
+
+// registerApplication({
+//   name: "@voyage/flights-app",
+//   app: () =>
+//     System.import(
+//       "@voyage/flights-app"
+//     ),
+//   activeWhen: ["/flights"],
+
+// });
+
+// registerApplication({
+//   name: "@voyage/hotels-app",
+//   app: () =>
+//     System.import(
+//       "@voyage/hotels-app"
+//     ),
+//   activeWhen: ["/hotels"],
+// });
+
+// Example Register Application
+// registerApplication({
+//   name: "@voyage/navbar",
+//   app: () => System.import("@voyage/navbar"),
+//   activeWhen: ["/"]
+// To Unique path activeWhen: (location) => location.pathname === "/flights",
+// });
+
+const routes = constructRoutes(document.querySelector('#single-spa-layout'))
+const applications = constructApplications({
+  routes,
+  loadApp({ name }) {
+    return System.import(name);
+  },
+});
+
+const layoutEngine = constructLayoutEngine({ routes, applications });
+applications.forEach(registerApplication);
+
+start({
+  urlRerouteOnly: true,
+});
